8d73aedc0ad00e52aab0ca833e7f60d57940f50b
Sahil Dussa <sahildussa457@gmail.com>
2024-10-21 08:32:16 +0530

    particular blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -2,7 +2,23 @@ import Blogs from "../models/Blog.js";
 
 export const getBlogs = async (req, res, next) => {
   try {
-    const response = await Blogs.find();
+    const response = await Blogs.find(
+      {},
+      { heading: 1, likes: 1, dislikes: 1 }
+    );
+    return res.status(200).json({
+      data: response,
+      message: "Blogs fetched successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+export const getParticularBlog = async (req, res, next) => {
+  try {
+    const response = await Blogs.findById(req.params.id);
+    console.log({ response });
     return res.status(200).json({
       data: response,
       message: "Blogs fetched successfully",
--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../middleware/verifyToken.js";
-import { getBlogs } from "../controllers/blog.js";
+import { getBlogs, getParticularBlog } from "../controllers/blog.js";
 
 router.get("/", verifyToken, getBlogs);
+router.get("/:id", verifyToken, getParticularBlog);
 
 export default router;
