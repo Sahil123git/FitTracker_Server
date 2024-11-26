b7cb55ac027716e4b7202d94e88937245059a7c9
Sahil Dussa <sahildussa457@gmail.com>
2024-11-26 08:24:12 +0530

    like and dislike feature

--- controllers/blog.js
+++ controllers/blog.js
@@ -25,6 +25,7 @@ export const getParticularBlog = async (req, res, next) => {
     });
     response._doc.userLike = likes?.like ?? false;
     response._doc.userDislike = likes?.dislike ?? false;
+    response._doc.likeId = likes?._id ?? null;
     console.log({ response });
     return res.status(200).json({
       data: response._doc,
@@ -39,8 +40,8 @@ export const likeOrDislikeBlog = async (req, res, next) => {
   try {
     const prevLike = req.body.prevLike;
     const prevDislike = req.body.prevDislike;
-    const like = req.body.userLike;
-    const dislike = req.body.userDislike;
+    let like = req.body.userLike;
+    let dislike = req.body.userDislike;
 
     let likeCnt = req.body.likes,
       dislikeCnt = req.body.dislikes;
@@ -48,38 +49,50 @@ export const likeOrDislikeBlog = async (req, res, next) => {
     if (prevLike !== like) {
       if (like) {
         likeCnt += 1;
+        if (dislike === true) {
+          dislikeCnt -= 1;
+          dislike = false;
+        }
       } else {
         likeCnt -= 1;
       }
     } else if (prevDislike !== dislike) {
       if (dislike) {
         dislikeCnt += 1;
+        if (like === true) {
+          likeCnt -= 1;
+          like = false;
+        }
       } else {
         dislikeCnt -= 1;
       }
     }
     const blogData = { ...req.body, likes: likeCnt, dislikes: dislikeCnt };
-    const response = await Blogs.findByIdAndUpdate(req.body.blogId, blogData);
+    const response = await Blogs.findByIdAndUpdate(req.body.blogId, blogData, {
+      new: true,
+    });
 
     let userReaction;
     if (req.body.likeId) {
-      userReaction = await Reactions.findByIdAndUpdate(req.body.likeId, {
-        like: req.body.like,
-        dislike: req.body.dislike,
-      });
+      userReaction = await Reactions.findByIdAndUpdate(
+        req.body.likeId,
+        {
+          like: like,
+          dislike: dislike,
+        },
+        { new: true }
+      );
     } else {
-      console.log("reached here");
       userReaction = await Reactions.create({
         blogId: req.body.blogId,
         userId: req.user.id,
-        like: req.body.userLike,
-        dislike: req.body.userDislike,
+        like: like,
+        dislike: dislike,
       });
     }
-    console.log({ response, userReaction });
     response._doc.userLike = userReaction.like;
     response._doc.userDislike = userReaction.dislike;
-    // console.log(req.params.id, User:req.user);
+    response._doc.likeId = userReaction._id;
 
     return res.status(200).json({
       data: response,
